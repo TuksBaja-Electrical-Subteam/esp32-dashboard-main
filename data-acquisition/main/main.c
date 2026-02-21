ce4d30e302461112b168394239fe26c75adadfa9
Moagi <53832997+Ev1dentSnow@users.noreply.github.com>
2026-02-21 21:40:29 +0200

    add: started ESP-NOW implementation for data acquisition ESP

--- data-acquisition/main/main.c
+++ data-acquisition/main/main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include "esp_now.h"
+
+const uint8_t PEER_MAC_ADDRESS = 30 : C9 : 22 : 32 : CF : F4;
+bool peer_connected = false;
+
+void initialize(void)
+{
+    esp_now_init();
+    esp_now_register_recv_cb(receive_data);
+    esp_now_register_send_cb(send_data);
+    esp_now_add_peer(PEER_MAC_ADDRESS);
+}
+
+void receive_data(void)
+{
+}
+
+void send_data(void)
+{
+    esp_err_t result = esp_now_send("", 8, 8);
+}
 
 void app_main(void)
 {
+}
 
+void reconnect_to_peer(void)
+{
 }
